4496eabb5df261831e01a1ca7aa02aa00bb3a455
akhilesh <akhileshc@planystech.com>
2024-12-09 00:31:17 +0530

    Add the read method

--- Devices/SerialDevice.cpp
+++ Devices/SerialDevice.cpp
@@ -1,36 +1,50 @@
 #include "SerialDevice.hpp"
 
-SerialDevice::SerialDevice(boost::asio::io_context &io_context, std::string devicePort, uint baudrate)
-    :serial_port(io_context), baudrate(baudrate)
+SerialDevice::SerialDevice(boost::asio::io_context &io_context, std::string devicePort, uint device_baudrate)
+    : serial_port(io_context), device_baudrate(device_baudrate), is_device_running(false)
 {
-    this->is_running=false;
-}
-
-void SerialDevice::Initialize()
-{
-    if(!serial_port.is_open())
-        serial_port.open(this->device_port);
-    serial_port.set_option(boost::asio::serial_port::baud_rate(baudrate));
-    serial_port.set_option(boost::asio::serial_port::parity::none);
-    serial_port.set_option(boost::asio::serial_port::character_size(8));
-    serial_port.set_option(boost::asio::serial_port::flow_control::none);
-    serial_port.set_option(boost::asio::serial_port::stop_bits::one);
-    this->is_running=true;
-}
-
-std::string SerialDevice::ReadOneLine()
-{
-    if(!this->is_running)
+    try
     {
-        std::cerr<<"File Name: " __FILE__<< " Method Name: " <<__func__<< "Error: "<<this->device_port<<" is not readable.";
+        if (!serial_port.is_open())
+            serial_port.open(this->device_port);
+        serial_port.set_option(boost::asio::serial_port::baud_rate(device_baudrate));
+        serial_port.set_option(boost::asio::serial_port::parity::none);
+        serial_port.set_option(boost::asio::serial_port::character_size(8));
+        serial_port.set_option(boost::asio::serial_port::flow_control::none);
+        serial_port.set_option(boost::asio::serial_port::stop_bits::one);
+        this->is_device_running = true;
+    }
+    catch (std::exception ex)
+    {
+        std::cerr << "File Name: " __FILE__ << " Method Name: " << __func__ << "Error: " << this->device_port << " is failed to initialize and exception message is " + std::string(ex.what());
+        this->is_device_running = false;
+    }
+}
+
+std::string SerialDevice::AsyncReadDevice()
+{
+    if (!this->is_device_running)
+    {
+        std::cerr << "File Name: " __FILE__ << " Method Name: " << __func__ << "Error: " << this->device_port << " is not readable.";
         return "";
     }
-    std::string reading_data;
-    serial_port.async_read_some(boost::asio::buffer(reading_data,this->one_time_read_length),handler);
-    return reading_data;
+    boost::asio::async_read(this->serial_port, boost::asio::buffer(*this->buffer_),
+                            [this, buffer = buffer_](const boost::system::error_code &error, std::size_t bytes_transferred)
+                            {
+                                this->ReadHandler(error, bytes_transferred, buffer);
+                            });
 }
 
-void handler(
-  const boost::system::error_code& error, // Result of operation.
-  std::size_t bytes_transferred           // Number of bytes read.
-);
+void SerialDevice::ReadHandler(const boost::system::error_code &error, std::size_t bytes_transferred, std::shared_ptr<std::array<char,1024>> buffer)
+{
+    if (!error)
+    {
+        std::string data(buffer->begin(), buffer->begin() + bytes_transferred);
+        OnDataReceived(data);
+        AsyncReadDevice();
+    }
+    else if (error != boost::asio::error::operation_aborted)
+    {
+        std::cerr << "File Name: " __FILE__ << " Method Name: " << __func__ << "Error: " << this->device_port << " is failed in reading.";
+    }
+}
